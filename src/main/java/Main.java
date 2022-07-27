a1d841317ebb7ecb6cf66fa9c5f4c8ae0bccac6b
Conner Gleason <connereg@optonline.net>
2022-07-27 11:48:59 -0400

    Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
@@ -5,13 +7,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newFixedThreadPool(1);
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+            executor.submit(new PrimeLogger(num));
         }
     }
+
+
 }
 
 class PrimeLogger implements Runnable {
@@ -24,5 +29,26 @@ class PrimeLogger implements Runnable {
     @Override
     public void run() {
         // print num if it is prime
+        if (isPrime()) {
+            System.out.println("this number is prime: " + num);
+        }
+        else {
+            System.out.println("This number is NOT prime: " + num);
+        }
     }
+
+    public boolean isPrime() {
+        if(num<=1)
+        {
+            return false;
+        }
+        for(int i=2 ;i<num/2; i++)
+        {
+            if((num%i)==0)
+                return  false;
+        }
+        return true;
+    }
+
+
 }
